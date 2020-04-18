5af15b2122b5a831b27e9114dc736f22c960bb0b
OrrinBlake <orrinblake@gmail.com>
2020-04-18 11:46:03 +0100

    Teleportation
    - Add teleport
    - Teleports when at reward

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,11 +1,26 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class Player : MonoBehaviour
-    {
-    private MazeCell _currentCell;
+{
+        public Transform teleportingPadTarget;
+        private MazeCell _currentCell;
 
-    public void SetLocation (MazeCell cell) {
-        _currentCell = cell;
-        transform.localPosition = cell.transform.localPosition + Vector3.up/2;
+        private void Start()
+        {
+            teleportingPadTarget = GameObject.Find("Enviroment/TeleportingPad").GetComponent<Transform>();
+        }
+
+        public void SetLocation (MazeCell cell) {
+            _currentCell = cell;
+            transform.localPosition = cell.transform.localPosition + Vector3.up/2;
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.CompareTag("Player"))
+            {
+                other.gameObject.transform.position = teleportingPadTarget.transform.position;
+            }
+        }
     }
-}
