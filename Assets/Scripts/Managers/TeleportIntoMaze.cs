71d42842250a696b1a50f8a96fbabb934a369327
OrrinBlake <orrinblake@gmail.com>
2020-04-20 12:17:42 +0100

    teleport active after button press

--- Assets/Scripts/Managers/TeleportIntoMaze.cs
+++ Assets/Scripts/Managers/TeleportIntoMaze.cs
@@ -5,18 +5,6 @@ namespace Managers
 {
     public class TeleportIntoMaze : MonoBehaviour
     {
-        public InsideMazeTeleporter teleporterCellLocation;
-        // Start is called before the first frame update
-        void Start()
-        {
-            
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-        
-        }
 
         private void OnTriggerEnter(Collider other)
         {
@@ -24,6 +12,7 @@ namespace Managers
             {
                 FindObjectOfType<AudioManager>().Play("Teleport");
                 other.gameObject.transform.position = GameObject.Find("InsideMazeTeleporter(Clone)").GetComponent<Transform>().position;
+                gameObject.SetActive(false);
             }
         }
     }
