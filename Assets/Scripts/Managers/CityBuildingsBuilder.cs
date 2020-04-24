85f61a372248cda43257ef21db65f2f58cfc3f74
OrrinBlake <orrinblake@gmail.com>
2020-04-24 14:41:50 +0100

    add city
    - Use simplified procedural city building
    - Build 1 layer of city around maze
    - Builds wall around maze to prevent player leaving

---
+++ Assets/Scripts/Managers/CityBuildingsBuilder.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CityBuildingsBuilder : MonoBehaviour
+{
+    public GameObject[] buildings;
+
+    public int wallWidth = 20;
+    public int mapHeight = 20;
+
+    public int spacing = 3;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        for (int h = -2; h < mapHeight - 97; h++)
+        {
+            for (int w = -30; w < wallWidth - 30; w += 60)
+            {
+                if (w == -30)
+                {
+                    Vector3 pos = new Vector3(w * 2, 0, h * spacing);
+                    int n = Random.Range(0, buildings.Length);
+                    Instantiate(buildings[n], pos, Quaternion.Euler(0, 90, 0));   
+                }
+                else
+                {
+                    Vector3 pos = new Vector3(w * 2, 0, h * spacing);
+                    int n = Random.Range(0, buildings.Length);
+                    Instantiate(buildings[n], pos, Quaternion.Euler(0, -90, 0));
+                }
+            }
+        }
+        
+        for (int h = -2; h < mapHeight - 97; h += 4)
+        {
+            for (int w = -3; w < wallWidth - 105; w++)
+            {
+                if (h == -2)
+                {
+                    Vector3 pos = new Vector3(w * spacing, 0, h * spacing);
+                    int n = Random.Range(0, buildings.Length);
+                    Instantiate(buildings[n], pos, Quaternion.identity);
+                }
+                else
+                {
+                    Vector3 pos = new Vector3(w * spacing, 0, h * spacing);
+                    int n = Random.Range(0, buildings.Length);
+                    Instantiate(buildings[n], pos, Quaternion.Euler(0, 180, 0));
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Managers/OuterwallsBuilder.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+
+public class OuterwallsBuilder : MonoBehaviour
+{
+
+    public GameObject[] walls;
+
+    public int wallWidth = 20;
+    public int mapHeight = 20;
+
+    public int spacing = 3;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        for (int i = 0; i < 2; i++)
+        {
+            for (int h = -40; h < mapHeight - 40; h += 79)
+            {
+                for (int w = -20; w < wallWidth - 20; w++)
+                {
+                    if (i == 0)
+                    {
+                        Vector3 pos = new Vector3(w * spacing, i, h * spacing);
+                        int n = Random.Range(0, walls.Length);
+                        Instantiate(walls[n], pos, Quaternion.identity);
+                    }
+                    else if(w % 2 == 0)
+                    {
+                        Vector3 pos = new Vector3(w * spacing, i, h * spacing);
+                        int n = Random.Range(0, walls.Length);
+                        Instantiate(walls[n], pos, Quaternion.identity);
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < 2; i++)
+        {
+            for (int h = -40; h < mapHeight - 40; h++)
+            {
+                for (int w = -20; w < wallWidth - 20; w += 40)
+                {
+                    if (i == 0)
+                    {
+                        Vector3 pos = new Vector3(w * spacing, i, h * spacing);
+                        int n = Random.Range(0, walls.Length);
+                        Instantiate(walls[n], pos, Quaternion.identity);
+                    }
+                    else if( h % 2 == 0)
+                    {
+                        Vector3 pos = new Vector3(w * spacing, i, h * spacing);
+                        int n = Random.Range(0, walls.Length);
+                        Instantiate(walls[n], pos, Quaternion.identity);
+                    }
+                }
+            }
+        }
+    }
+}
